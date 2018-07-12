054affcf601037eca56f5f4b4cfc72063afc8b5a
kaaboaye <kaaboaye@gmail.com>
2018-07-12 17:05:07 +0200

    add frame

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 export { VueDraggable } from './src/components/vue-draggable-resizable.vue';
+export { VueDraggableFrame } from './src/components/vue-draggable-frame.vue';
---
+++ src/components/vue-draggable-frame.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="vdr-frame">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import { matchesSelectorToParentElements } from '../utils/dom'
+
+export const VueDraggableFrame = {
+  replace: true,
+  name: 'VueDraggableFrame',
+  props: {},
+  methods: {},
+  computed: {},
+  watch: {}
+}
+
+export default VueDraggableFrame;
+</script>
+
+<style scoped>
+.vdr-frame {
+    position: relative;
+}
+</style>
